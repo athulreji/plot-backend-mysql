c570af4022609ba7f3b155fc47d52b20f722cfc4
adamsyy <adamoommenjacob@gmail.com>
2022-10-27 22:10:38 +0530

    s

--- controller/land/hotoptions.js
+++ controller/land/hotoptions.js
@@ -43,7 +43,6 @@ module.exports.hotoptions = async (req, res) => {
         "description":"One acre of unused land near town which we are planning to use for farming. We are looking for stock holders who can hold for a maximum of 3 years",
         "image": "https://images.unsplash.com/photo-1593642532972-7f1c2a8b3a1d?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8cGxheWdyb3VuZCUyMGxhbmR8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80",
         "cost": 100,
-        "remaining": 100,
             },
 }
 
